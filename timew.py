fcef1d30f04c3670c921359dca7c13342893597c
tkunic <tkunic@rocketmail.com>
2017-11-21 17:15:14 -0500

    Linted timew.py.

--- timew.py
+++ timew.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import threading
-import dateutil.parser, datetime, pytz
-import urllib
+import pytz
 
 from subprocess import check_output, CalledProcessError
 
 TIMEZONE = pytz.timezone("America/Toronto")
+
 
 class TimeWD:
     def __init__(self):
@@ -15,14 +15,13 @@ class TimeWD:
         self.current_task_update_status = "IDLE"
 
     def start(self):
-        self.__continuously_update_current_task_line()
-        self.__continuously_update_daily_total_line()
-
+        self._cts_update_current_task_line()
+        self._cts_update_daily_total_line()
 
     def get_current_task_line(self):
         return self.current_task_line
 
-    def update_current_task_line(self):
+    def _update_current_task_line(self):
         if self.current_task_update_status == "IDLE":
             self.current_task_update_status = "BUSY"
         else:
@@ -41,18 +40,18 @@ class TimeWD:
                 total=self.daily_total_line)
         except CalledProcessError:
             self.current_task_line = "No task running. [{total}]".format(total=self.daily_total_line)
-        except:
+        except: # Catch-all, don't crash i3bar
             self.current_task_line = "Current task query failed."
 
         self.current_task_update_status = "IDLE"
 
-    def update_daily_total_line(self):
+    def _update_daily_total_line(self):
         if self.current_task_update_status != "IDLE": # HACK
             return
 
         try:
             lines = check_output(["timew", "day"]).split('\n')
-            lines = [_.strip() for _ in lines]
+            lines = [line.strip() for line in lines]
 
             for line in lines:
                 if 'Tracked' in line:
@@ -63,16 +62,14 @@ class TimeWD:
         except:
             self.current_task_line = "Daily total query failed."
 
-    def __continuously_update_current_task_line(self):
-        self.update_current_task_line()
-        t = threading.Timer(5, self.__continuously_update_current_task_line)
-        t.daemon = True
+    def _cts_update_current_task_line(self):
+        self._update_current_task_line()
+        t = threading.Timer(5, self._cts_update_current_task_line)
         t.start()
 
-    def __continuously_update_daily_total_line(self):
-        self.update_daily_total_line()
-        t = threading.Timer(31, self.__continuously_update_daily_total_line)
-        t.daemon = True
+    def _cts_update_daily_total_line(self):
+        self._update_daily_total_line()
+        t = threading.Timer(31, self._cts_update_daily_total_line)
         t.start()
 
 if __name__ == "__main__":
